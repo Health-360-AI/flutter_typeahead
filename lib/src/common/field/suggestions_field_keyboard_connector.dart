42392ba7eb7707c37b90791d2a7a600d1dacd129
Karar_faleh <karrar.falih@gmail.com>
2025-11-30 23:37:05 +0300

    refactor: remove keyboard visibility dependency and simplify suggestions field connector

--- lib/src/common/field/suggestions_field_keyboard_connector.dart
+++ lib/src/common/field/suggestions_field_keyboard_connector.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
-import 'package:flutter_typeahead/src/common/base/connector_widget.dart';
 import 'package:flutter_typeahead/src/common/base/suggestions_controller.dart';
 
 /// Hides the suggestions box when the keyboard is closed.
@@ -18,16 +16,6 @@ class SuggestionsFieldKeyboardConnector<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConnectorWidget(
-      // [KeyboardVisibilityController] is a singleton.
-      value: KeyboardVisibilityController(),
-      connect: (value) => value.onChange.listen((visible) {
-        if (!visible && hideWithKeyboard) {
-          controller.close();
-        }
-      }),
-      disconnect: (value, key) => key?.cancel(),
-      child: child,
-    );
+    return child;
   }
 }
